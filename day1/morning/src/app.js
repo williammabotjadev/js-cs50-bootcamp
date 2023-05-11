3b22decccd8bf1dc2033599c632b0683200e64af
William Mabotja <williammabotja@Admins-MacBook-Pro.local>
2023-05-11 18:17:50 +0200

    heads, tails, day1

--- day1/morning/src/app.js
+++ day1/morning/src/app.js
@@ -1,42 +1,28 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var coinState = true;
+let coinState = true;
 
 app.get('/', function (req, res) {
-    res.send('Hello, World!');
-});
+    res.send('Hello, World!')
+})
 
-app.get('/hello', function (req, res) {
+app.get('/hello', (req, res) => {
     console.log('hello');
     res.send('hello');
 });
 
-app.get('/coin', function(req, res) {
+app.get('/coin', (req, res) => {
     var randomNumber = Math.random();
-    if (randomNumber > .5) {
-        var coin = 'Heads';
-    } else {
-        var coin = 'Tails';
-    }
-
-    res.send(coin);
+    var coin = randomNumber > .5 ? "Heads" : "Tails";
+    res.send(coin); 
 });
 
-app.get('/getcoinstate', function(req, res) {
-    if (coinState) {
-        res.send('Heads');
-    } else {
-        res.send('Tails');
-    }
-});
-
-app.get('/flipcoin', function(req, res) {
-    coinState = !!Math.round(Math.random())
-    res.send();
-});
-
+app.get('/getCoinState', (req, res) => {
+    let state = coinState === true ? "Heads" : "Tails";
+    res.send(state);
+})
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!')
 });
